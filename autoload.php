e6d699eb1337656d414fca6d064d4930a28eca79
Jason smulan <jasonsmulan@gmail.com>
2013-02-22 20:58:19 -0700

    Changed the Ajax

--- autoload.php
+++ autoload.php
@@ -2,17 +2,9 @@
     <head>
         <script type="text/javascript" src="js/jquery-1.9.1.js"></script>
         <script>
-            function myFunction(){
-                document.getElementById("demo").innerHTML="Hello World";
-            }
-
             function changeTitle(){
                 var jqxhr = $.ajax( "/jsmulan-sandbox/content.php" )
-                .done(function() { alert("success"); })
-                .fail(function() { alert("error"); })
-                .always(function() { alert("complete"); });
-                
-                alert( jqxhr );
+                .done(function(msg) { $("#title").append(msg); });
             }
         </script>
         <title>
@@ -20,12 +12,7 @@
         </title>
     </head>
     <body>
-        <div id="title">
-            
-        </div>
-        <p id="demo"></p>
-        <a href="/jsmulan-sandbox/content.php">content</a>
+        <div id="title"></div>
         <button onclick="changeTitle()">Click</button>
-        <button onclick="myFunction()">Click me</button>
     </body>
 </html>

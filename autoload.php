a4779be83ccec2c7aa29fe9c2e92bf067db06052
Jason smulan <jasonsmulan@gmail.com>
2013-02-22 20:49:12 -0700

    Changed the js

--- autoload.php
+++ autoload.php
@@ -1,16 +1,19 @@
 <html>
     <head>
-        <script type="text/javascript" src="js/jquery-1.9.1.js">
-             $(document).ready(
-                function changeTitle(){
-                    var jqxhr = $.ajax( "/jsmulan-sandbox/content.php" )
-                    .done(function() { alert("success"); })
-                    .fail(function() { alert("error"); })
-                    .always(function() { alert("complete"); });
-                    
-                    alert( jqxhr );
-                }
-            );
+        <script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+        <script>
+            function myFunction(){
+                document.getElementById("demo").innerHTML="Hello World";
+            }
+
+            function changeTitle(){
+                var jqxhr = $.ajax( "/jsmulan-sandbox/content.php" )
+                .done(function() { alert("success"); })
+                .fail(function() { alert("error"); })
+                .always(function() { alert("complete"); });
+                
+                alert( jqxhr );
+            }
         </script>
         <title>
             Autoloader Test
@@ -20,7 +23,9 @@
         <div id="title">
             
         </div>
+        <p id="demo"></p>
         <a href="/jsmulan-sandbox/content.php">content</a>
-        <button onclick="changeTitle()">Click</button> 
+        <button onclick="changeTitle()">Click</button>
+        <button onclick="myFunction()">Click me</button>
     </body>
 </html>

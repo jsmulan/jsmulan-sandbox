453f6a512227d746700de295e50730ed9ef9ea0c
Jason smulan <jasonsmulan@gmail.com>
2013-02-25 13:48:16 -0700

    Added the file write and the 32 character

--- Avrotest.php
+++ Avrotest.php
@@ -8,17 +8,22 @@
             $results.= $char;
         }
     }
-var_dump( $results );
+    $file = fopen( 'output.txt', 'w' );
+    fwrite( $file, $results );
+    fclose( $file );
 ?>
 <html>
     <head>
         <title>
-            
+            Avro Test
         </title>
     </head>
     <body>
+        <?php if( FALSE == empty( $results ) { ?>
+        <h2><?php echo $results; ?></h2>
+        <?php } ?>
         <form action="Avrotest.php" method="GET">
-            <input type="text" name="characters" id="characters">
+            <input type="text" name="characters" id="characters" maxlength="32" >
             
             <input type="submit" value="Save">
         </form>

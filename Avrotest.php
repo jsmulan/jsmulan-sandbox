006fbceb9d07b8ecbd1fbe927e3568ed02d1550b
Jason smulan <jasonsmulan@gmail.com>
2013-02-25 13:21:22 -0700

    Start of the Avro test

---
+++ Avrotest.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>
+            
+        </title>
+    </head>
+    <body>
+        <form action="Avrotest.php">
+            <input type="input">
+            
+            <input type="submit">
+        </form>
+    </body>
+</html>
